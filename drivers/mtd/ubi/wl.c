74d056be0cec745e2d280440cb58be7cb64df2cc
Richard Weinberger <richard@nod.at>
2015-11-05 23:56:53 +0100

    UBI: Introduce prepare_erase_work()
    Split ubi_work creation and scheduling code such that
    we can re-use the creation code where we schedule ubi work
    on our own.
    
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- drivers/mtd/ubi/wl.c
+++ drivers/mtd/ubi/wl.c
@@ -569,6 +569,37 @@ static int erase_worker(struct ubi_device *ubi, struct ubi_work *wl_wrk,
 			int shutdown);
 
 /**
+ * prepare_erase_work - prepare an erase work.
+ * @ubi: UBI device description object
+ * @e: the WL entry of the physical eraseblock to erase
+ * @vol_id: the volume ID that last used this PEB
+ * @lnum: the last used logical eraseblock number for the PEB
+ * @torture: if the physical eraseblock has to be tortured
+ *
+ * This function returns a struct ubi_work in case of success
+ * and an ERR_PTR(%-ENOMEM) in case of failure.
+ */
+static struct ubi_work *prepare_erase_work(struct ubi_wl_entry *e, int vol_id,
+					   int lnum, int torture)
+{
+	struct ubi_work *wl_wrk;
+
+	ubi_assert(e);
+
+	wl_wrk = kmalloc(sizeof(struct ubi_work), GFP_NOFS);
+	if (!wl_wrk)
+		return ERR_PTR(-ENOMEM);
+
+	wl_wrk->func = &erase_worker;
+	wl_wrk->e = e;
+	wl_wrk->vol_id = vol_id;
+	wl_wrk->lnum = lnum;
+	wl_wrk->torture = torture;
+
+	return wl_wrk;
+}
+
+/**
  * schedule_erase - schedule an erase work.
  * @ubi: UBI device description object
  * @e: the WL entry of the physical eraseblock to erase
@@ -582,22 +613,13 @@ static int erase_worker(struct ubi_device *ubi, struct ubi_work *wl_wrk,
 static int schedule_erase(struct ubi_device *ubi, struct ubi_wl_entry *e,
 			  int vol_id, int lnum, int torture)
 {
-	struct ubi_work *wl_wrk;
-
-	ubi_assert(e);
+	struct ubi_work *wl_wrk = prepare_erase_work(e, vol_id, lnum, torture);
 
 	dbg_wl("schedule erasure of PEB %d, EC %d, torture %d",
 	       e->pnum, e->ec, torture);
 
-	wl_wrk = kmalloc(sizeof(struct ubi_work), GFP_NOFS);
-	if (!wl_wrk)
-		return -ENOMEM;
-
-	wl_wrk->func = &erase_worker;
-	wl_wrk->e = e;
-	wl_wrk->vol_id = vol_id;
-	wl_wrk->lnum = lnum;
-	wl_wrk->torture = torture;
+	if (IS_ERR(wl_wrk))
+		return PTR_ERR(wl_wrk);
 
 	schedule_ubi_work(ubi, wl_wrk);
 	return 0;
