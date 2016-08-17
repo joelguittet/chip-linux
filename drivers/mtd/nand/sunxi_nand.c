69873d9634af917d94476cd207780bbab1008cd1
Boris Brezillon <boris.brezillon@free-electrons.com>
2016-08-17 11:45:20 +0200

    mtd: nand: sunxi: enable NAND_USE_BOUNCE_BUFFER when DMA is activated
    Some buffers are not suitable for DMA operations (like vmalloced
    buffers). Commit 75dddc2d52f1 "scatterlist: add sg_alloc_table_from_buf()
    helper" was trying to address this limitation, but some concern were
    raised about this approach.
    
    Just force bounce buffer usage instead of trying to be smart when the
    buffer passed by the MTD user is not suitable for DMA operations.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@free-electrons.com>

--- drivers/mtd/nand/sunxi_nand.c
+++ drivers/mtd/nand/sunxi_nand.c
@@ -1827,6 +1827,7 @@ static int sunxi_nand_hw_ecc_ctrl_init(struct mtd_info *mtd,
 		ecc->read_page = sunxi_nfc_hw_ecc_read_page_dma;
 		ecc->read_subpage = sunxi_nfc_hw_ecc_read_subpage_dma;
 		ecc->write_page = sunxi_nfc_hw_ecc_write_page_dma;
+		nand->options |= NAND_USE_BOUNCE_BUFFER;
 	} else {
 		ecc->read_page = sunxi_nfc_hw_ecc_read_page;
 		ecc->read_subpage = sunxi_nfc_hw_ecc_read_subpage;
